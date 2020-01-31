0739ccfac49ff3fd51dacb6a372c3864467e3a51
cstuser <christian_kfoury558@hotmail.ca>
2020-01-31 13:48:18 -0500

    MetertoFeet si in

---
+++ MeterToFeet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MetersToFeet{
+public static void main(String[] args) {
+	Scanner kb = new Scanner(System.in);
+	int meters = kb.next.Int();
+	int feet = meters * 3.2808399;
+
+	System.out.print("1 meter = 3.2808399 feet" + meters " meters "  +  " = " + feet " feet ")
+
+
+
+}
+
+
+}
