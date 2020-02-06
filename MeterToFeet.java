84221902250397840bb8244e3036dba442eda6e7
christiankfoury <60350618+christiankfoury@users.noreply.github.com>
2020-02-06 13:39:42 -0500

    Update MeterToFeet.java

--- MeterToFeet.java
+++ MeterToFeet.java
@@ -1,15 +1,11 @@
 import java.util.*;
-public class MeterToFeet{
+public class MetersToFeet{
 public static void main(String[] args) {
 	Scanner kb = new Scanner(System.in);
+	System.out.println("Enter a value in meters");
 	double meters = kb.nextInt();
 	double feet = meters * 3.2808399;
 
 	System.out.print("1.0 meter = 3.2808399 feet " + meters + " meters "  +  " = " + feet + " feet ");
-
-
-
 }
-
-
 }
